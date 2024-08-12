449e70d3d239a664b5a08379428a1e3a62721eff
Adejare <adejaremarvel@gmail.com>
2024-08-12 14:03:01 +0100

    ``updates to introptsactie

---
+++ intropractice.py
@@ -0,0 +1,28 @@
+# program to extract tables and use numpy and pandas to manipulate te data and use matplot lib to plot charts where necessary
+# as well as using loops to  iterate through a data set to identify certian elements as well  as replace
+# also using numpy to perform calculations.
+
+# first create a web scrapper
+
+import pandas as pd
+import numpy as np
+from bs4 import BautifulSoup
+
+# soup = BeautifulSoup(res.content, 'html-parser')
+# df = pd.read_html(url)
+# table 2 = df[1]
+# table 2 = df(2)
+
+
+def fetchApi(url):
+    pass
+    # 
+
+
+
+
+
+
+fetchApi(url)
+
+
